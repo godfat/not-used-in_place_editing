2feecd854f3c269f871e1fd20baee4b549162b06
godfat <godfat@godfat.org>
2008-09-15 18:10:06 +0800

    use .underscore instead of .downcase to work better with the convention.
      credits to maciej http://github.com/maciej.
      see the comment for detail:
        http://github.com/godfat/in_place_editing/commit/1457991dd881d86f8e0ecf74a150a3097772e3c3#lib/in_place_macros_helper.rb-P10

--- lib/in_place_macros_helper.rb
+++ lib/in_place_macros_helper.rb
@@ -70,7 +70,7 @@ module InPlaceMacrosHelper
   # Renders the value of the specified object and method with in-place editing capabilities.
   def in_place_editor_field(object, method, tag_options = {}, in_place_editor_options = {})
     if object.kind_of?(::ActiveRecord::Base)
-      name = object.class.to_s.downcase
+      name = object.class.to_s.underscore
       tag = ::ActionView::Helpers::InstanceTag.new(name, method, self, nil, object)
     else
       name = object
